2bf99d986ae90a0b4193ea8c816c24891ff86041
Daniel <curveballdaniel@gmail.com>
2021-01-30 16:52:43 -0500

    Modernize commands in index.js, add simple extra commands

---
+++ commands/chad.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'statera',
+	description: 'Only gigachads hold statera',
+	execute(message) {
+		message.channel.send('OOOOOHHHHH I\'M ABOUT TO POOOOOOOOOOOOOOOL');
+	},
+};
--- index.js
+++ index.js
@@ -1,20 +1,39 @@
+const fs = require('fs');
 const dotenv = require('dotenv');
-dotenv.config();
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
-// const config = require('./config.json');
+dotenv.config();
+const { prefix } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// load command files from /commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-	console.log(message.content);
-  if (message.content === '!ping') {
-  	// send back "Pong." to the channel the message was sent in
-  	message.channel.send('Pong.');
-  }
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
 // log bot into discord
